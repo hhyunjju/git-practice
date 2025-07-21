1bcd6c519fb6b726244e7a7cd6994a321f92fd5a
hhyunjju <hhyunjju0618@gmail.com>
2025-07-21 16:31:46 +0900

    [feature/func-temp] 로그 롬복 추가

--- demo2/src/main/java/com/example/demo/DemoService.java
+++ demo2/src/main/java/com/example/demo/DemoService.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class DemoService {

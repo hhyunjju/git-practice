3d854c96a7735dcdddaca391d255afa632627ec0
hhyunjju <hhyunjju0618@gmail.com>
2025-07-21 10:34:09 +0900

    main 내용 변경3

--- demo2/src/main/java/com/example/demo/DemoService.java
+++ demo2/src/main/java/com/example/demo/DemoService.java
@@ -1,4 +1,8 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class DemoService {
+    // demo service 작성
 }
--- demo2/src/main/java/com/example/demo/PopupService.java
+++ demo2/src/main/java/com/example/demo/PopupService.java
@@ -1,4 +1,8 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PopupService {
+    // popup service 작성
 }

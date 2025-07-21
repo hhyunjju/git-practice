31a7e14caa1e9e224dc31263b2b891c1f42a5530
hhyunjju <hhyunjju0618@gmail.com>
2025-07-21 10:38:31 +0900

    [feature/popup1] popup기능 개발

--- demo2/src/main/java/com/example/demo/PopupService.java
+++ demo2/src/main/java/com/example/demo/PopupService.java
@@ -2,7 +2,15 @@ package com.example.demo;
 
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class PopupService {
     // popup service 작성
+    private static final Map<String, String> popupStorage = new HashMap<>();
+
+    public void add(String id, String content) {
+        popupStorage.put(id, content);
+    }
 }
